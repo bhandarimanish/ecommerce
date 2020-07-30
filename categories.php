e014bf600768b57b98abb0c643b3391b34886b93
bhandarimanish <vandarymannis0@gmail.com>
2020-07-30 10:45:50 +0545

    My 1st commit

---
+++ categories.php
@@ -0,0 +1,265 @@
+<?php require('top.php')?>
+   <div class="body__overlay"></div>
+        <!-- Start Offset Wrapper -->
+        <div class="offset__wrapper">
+            <!-- Start Search Popap -->
+            <div class="search__area">
+                <div class="container" >
+                    <div class="row" >
+                        <div class="col-md-12" >
+                            <div class="search__inner">
+                                <form action="#" method="get">
+                                    <input placeholder="Search here... " type="text">
+                                    <button type="submit"></button>
+                                </form>
+                                <div class="search__close__btn">
+                                    <span class="search__close__btn_icon"><i class="zmdi zmdi-close"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Search Popap -->
+            <!-- Start Cart Panel -->
+            <div class="shopping__cart">
+                <div class="shopping__cart__inner">
+                    <div class="offsetmenu__close__btn">
+                        <a href="#"><i class="zmdi zmdi-close"></i></a>
+                    </div>
+                    <div class="shp__cart__wrap">
+                        <div class="shp__single__product">
+                            <div class="shp__pro__thumb">
+                                <a href="#">
+                                    <img src="images/product-2/sm-smg/1.jpg" alt="product images">
+                                </a>
+                            </div>
+                            <div class="shp__pro__details">
+                                <h2><a href="product-details.html">BO&Play Wireless Speaker</a></h2>
+                                <span class="quantity">QTY: 1</span>
+                                <span class="shp__price">$105.00</span>
+                            </div>
+                            <div class="remove__btn">
+                                <a href="#" title="Remove this item"><i class="zmdi zmdi-close"></i></a>
+                            </div>
+                        </div>
+                        <div class="shp__single__product">
+                            <div class="shp__pro__thumb">
+                                <a href="#">
+                                    <img src="images/product-2/sm-smg/2.jpg" alt="product images">
+                                </a>
+                            </div>
+                            <div class="shp__pro__details">
+                                <h2><a href="product-details.html">Brone Candle</a></h2>
+                                <span class="quantity">QTY: 1</span>
+                                <span class="shp__price">$25.00</span>
+                            </div>
+                            <div class="remove__btn">
+                                <a href="#" title="Remove this item"><i class="zmdi zmdi-close"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="shoping__total">
+                        <li class="subtotal">Subtotal:</li>
+                        <li class="total__price">$130.00</li>
+                    </ul>
+                    <ul class="shopping__btn">
+                        <li><a href="cart.html">View Cart</a></li>
+                        <li class="shp__checkout"><a href="checkout.html">Checkout</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End Cart Panel -->
+        </div>
+        <!-- End Offset Wrapper -->
+        <!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner">
+                                <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.html">Home</a>
+                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                                  <span class="breadcrumb-item active">Products</span>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Bradcaump area -->
+        <!-- Start Product Grid -->
+        <section class="htc__product__grid bg__white ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-xs-12">
+                        <div class="htc__product__rightidebar">
+                            <div class="htc__grid__top">
+                                <div class="htc__select__option">
+                                    <select class="ht__select">
+                                        <option>Default softing</option>
+                                        <option>Sort by popularity</option>
+                                        <option>Sort by average rating</option>
+                                        <option>Sort by newness</option>
+                                    </select>
+                                </div>
+                                
+                                <!-- Start List And Grid View -->
+                                <ul class="view__mode" role="tablist">
+                                    <li role="presentation" class="grid-view active"><a href="#grid-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-grid"></i></a></li>
+                                    <li role="presentation" class="list-view"><a href="#list-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-view-list"></i></a></li>
+                                </ul>
+                                <!-- End List And Grid View -->
+                            </div>
+                            <!-- Start Product View -->
+                            <div class="row">
+                                <div class="shop__grid__view__wrap">
+                                    <div role="tabpanel" id="grid-view" class="single-grid-view tab-pane fade in active clearfix">
+                                        <!-- Start Single Product -->
+                                        <div class="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+                                            <div class="category">
+                                                <div class="ht__cat__thumb">
+                                                    <a href="product-details.html">
+                                                        <img src="images/product/1.jpg" alt="product images">
+                                                    </a>
+                                                </div>
+                                                <div class="fr__hover__info">
+                                                    <ul class="product__action">
+                                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="fr__product__inner">
+                                                    <h4><a href="product-details.html">Largest Water Pot</a></h4>
+                                                    <ul class="fr__pro__prize">
+                                                        <li class="old__prize">$30.3</li>
+                                                        <li>$25.9</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- End Single Product -->
+                                      
+                                    </div>
+                                    <div role="tabpanel" id="list-view" class="single-grid-view tab-pane fade clearfix">
+                                        <div class="col-xs-12">
+                                            <div class="ht__list__wrap">
+                                                <!-- Start List Product -->
+                                                <div class="ht__list__product">
+                                                    <div class="ht__list__thumb">
+                                                        <a href="product-details.html"><img src="images/product-2/pro-1/1.jpg" alt="product images"></a>
+                                                    </div>
+                                                    <div class="htc__list__details">
+                                                        <h2><a href="product-details.html">Product Title Here </a></h2>
+                                                        <ul  class="pro__prize">
+                                                            <li class="old__prize">$82.5</li>
+                                                            <li>$75.2</li>
+                                                        </ul>
+                                                        <ul class="rating">
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                        </ul>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisLorem ipsum dolor sit amet, consec adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqul Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                                        <div class="fr__list__btn">
+                                                            <a class="fr__btn" href="cart.html">Add To Cart</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End List Product -->
+                                                <!-- Start List Product -->
+                                                <div class="ht__list__product">
+                                                    <div class="ht__list__thumb">
+                                                        <a href="product-details.html"><img src="images/product-2/pro-1/2.jpg" alt="product images"></a>
+                                                    </div>
+                                                    <div class="htc__list__details">
+                                                        <h2><a href="product-details.html">Product Title Here </a></h2>
+                                                        <ul  class="pro__prize">
+                                                            <li class="old__prize">$82.5</li>
+                                                            <li>$75.2</li>
+                                                        </ul>
+                                                        <ul class="rating">
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                        </ul>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisLorem ipsum dolor sit amet, consec adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqul Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                                        <div class="fr__list__btn">
+                                                            <a class="fr__btn" href="cart.html">Add To Cart</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End List Product -->
+                                                <!-- Start List Product -->
+                                                <div class="ht__list__product">
+                                                    <div class="ht__list__thumb">
+                                                        <a href="product-details.html"><img src="images/product-2/pro-1/3.jpg" alt="product images"></a>
+                                                    </div>
+                                                    <div class="htc__list__details">
+                                                        <h2><a href="product-details.html">Product Title Here </a></h2>
+                                                        <ul  class="pro__prize">
+                                                            <li class="old__prize">$82.5</li>
+                                                            <li>$75.2</li>
+                                                        </ul>
+                                                        <ul class="rating">
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                        </ul>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisLorem ipsum dolor sit amet, consec adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqul Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                                        <div class="fr__list__btn">
+                                                            <a class="fr__btn" href="cart.html">Add To Cart</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End List Product -->
+                                                <!-- Start List Product -->
+                                                <div class="ht__list__product">
+                                                    <div class="ht__list__thumb">
+                                                        <a href="product-details.html"><img src="images/product-2/pro-1/4.jpg" alt="product images"></a>
+                                                    </div>
+                                                    <div class="htc__list__details">
+                                                        <h2><a href="product-details.html">Product Title Here </a></h2>
+                                                        <ul  class="pro__prize">
+                                                            <li class="old__prize">$82.5</li>
+                                                            <li>$75.2</li>
+                                                        </ul>
+                                                        <ul class="rating">
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                            <li class="old"><i class="icon-star icons"></i></li>
+                                                        </ul>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipisLorem ipsum dolor sit amet, consec adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqul Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                                        <div class="fr__list__btn">
+                                                            <a class="fr__btn" href="cart.html">Add To Cart</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!-- End List Product -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Product View -->   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+<?php require('footer.php')?>        
